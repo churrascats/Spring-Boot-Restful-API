a6136de7f3934be59c0e873bfb96977996ebc769
Igor Batista <igorsb11@hotmail.com>
2021-04-07 11:03:32 -0300

    Depency injection implemented usin Bean's annotations in order to estabilish communication between API Layer and Service Layer,

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(new Student(1L, "Igor", "igorsb11@hotmail.com", LocalDate.of(1997, Month.JULY, 18), 21));
